598b3741215ba3a3d7186fc956da9dbc167392b5
ccfine <ccfine24@163.com>
2019-03-04 17:17:03 +0800

    修改pm2文件配置

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,8 +5,8 @@ module.exports = {
       host: "129.204.68.85",
       ref: "origin/master",
       repo: "https://github.com/ccfine/ccfine24.git",
-      path: "/ccfine/project",
-      "post-deploy": "cd /ccfine/deployment/ccfine24 && rm -f index.html"
+      path: "/ccfine/project/ccfine24",
+      "post-deploy": "cd /ccfine/deployment/ccfine24 && rm -f index.html && cd /ccfine/project/ccfine24/current && cp index.html /ccfine/deployment/ccfine24"
     }
   }
 }
